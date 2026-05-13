8f87874620aa4b6808745678abe54b111dc6e517
Sreekar Kondapalli <sreekarkondapally11@gmail.com>
2026-05-13 09:46:57 +0530

    Add files via upload

---
+++ DBConnection.java
@@ -0,0 +1,13 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class DBConnection {
+    public static Connection getConnection() throws Exception {
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        return DriverManager.getConnection(
+            "jdbc:mysql://localhost:3306/crud",
+            "root",
+            "123456"
+        );
+    }
+}
